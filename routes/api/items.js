67970fb648157b8b4429038ca8e37eb27387777f
Soufiane Elharem <44909504+Selharem@users.noreply.github.com>
2021-04-24 17:03:30 +0000

    Implement DELETE and POST

--- routes/api/items.js
+++ routes/api/items.js
@@ -1,21 +1,35 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 //Item model
-const Item = require('../../models/Item');
+const Item = require("../../models/Item");
 
 // @route GET api/items
 // @desc  GET all items
 // @access Public
-router.get('/' , (req, res) => {
-
-    Item.find()
-        .sort({ date : -1 })
-        .then( items => res.json(items) )
-
+router.get("/", (req, res) => {
+  Item.find()
+    .sort({ date: -1 })
+    .then((items) => res.json(items));
 });
 
+// @route POST api/items
+// @desc  Create a Post
+// @access Public
+router.post("/", (req, res) => {
+  const newItem = new Item({
+    name: req.body.name,
+  });
+
+  newItem.save().then((items) => res.json(items));
+});
+
+// @route DELETE api/items/:id
+// @desc  DELETE an item
+// @access Public
+router.delete("/:id", (req, res) => {
+  Item.findById(req.params.id)
+    .then((item) => item.remove().then(() => res.json({ sucess: true })))
+    .catch((err) => res.status(404).json({ sucess: false }));
+});
 module.exports = router;
-
-
-
